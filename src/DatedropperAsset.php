faee094beadaa78de53550d7e263c750a6bc8be0
simialbi <simi.albi@outlook.com>
2020-08-31 17:52:35 +0200

    init

---
+++ src/DatedropperAsset.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package yii2-widget-datedropper
+ * @author Simon Karlen <simi.albi@outlook.com>
+ */
+
+namespace simialbi\yii2\datedropper;
+
+use simialbi\yii2\web\AssetBundle;
+
+/**
+ * Asset bundle for datedropper Widget
+ *
+ * @author Simon karlen <simi.albi@outlook.com>
+ */
+class DatedropperAsset extends AssetBundle
+{
+    /**
+     * {@inheritDoc}
+     */
+    public $js = [
+        'js/datedropper.pro.min.js'
+    ];
+}
